b99b769e19cda963376f5ab990060dea74f8fee8
adesvall <adesvall@student.42.fr>
2020-11-26 23:37:36 +0100

    re

--- get_next_line/main.c
+++ get_next_line/main.c
@@ -6,7 +6,7 @@
 /*   By: adesvall <adesvall@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/25 03:02:21 by adesvall          #+#    #+#             */
-/*   Updated: 2020/11/26 18:08:40 by adesvall         ###   ########.fr       */
+/*   Updated: 2020/11/26 23:06:18 by adesvall         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,17 @@
 int	main(int argc, char **argv)
 {
 	int		fd;
-	int		fd2;
 	char	**line;
 
 	line = malloc(sizeof(char*));
 	(void)argc;
 	fd = open(argv[1], O_RDWR);
-	fd2 = open(argv[2], O_RDWR);
 	while (get_next_line(fd, line))
 	{
 		printf("Lfd1:%s\n", *line);
-		get_next_line(fd2, line);
-		printf("Lfd2:%s\n", *line);
+		free(*line);
 	}
+	free(*line);
+	free(line);
 	printf("Error or EOF\n");
 }
