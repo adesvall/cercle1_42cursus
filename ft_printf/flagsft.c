e52736211a736523a11420d56c1cae92b8e8fa24
adesvall <adesvall@student.42.fr>
2020-12-04 14:48:08 +0100

    forme

---
+++ ft_printf/flagsft.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   flagsft.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adesvall <adesvall@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/03 16:38:27 by adesvall          #+#    #+#             */
+/*   Updated: 2020/12/03 16:46:26 by adesvall         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+void	flag_minus(t_flags *flags)
+{
+	flags->minus = 1;
+	flags->zero = 0;
+}
+
+void	flag_digit(char c, t_flags *flags)
+{
+	if (flags->star == 1)
+		flags->width = 0;
+	flags->width = (flags->width * 10) + (c - '0');
+}
+
+void	flag_width(va_list args, t_flags *flags)
+{
+	flags->star = 1;
+	flags->width = va_arg(args, int);
+	if (flags->width < 0)
+	{
+		flags->minus = 1;
+		flags->width *= -1;
+	}
+}
+
+int		ft_flag_dot(const char *save, int start, t_flags *flags, va_list args)
+{
+	int i;
+
+	i = start;
+	i++;
+	if (save[i] == '*')
+	{
+		flags->dot = va_arg(args, int);
+		i++;
+	}
+	else
+	{
+		flags->dot = 0;
+		while (ft_isdigit(save[i]))
+			flags->dot = (flags->dot * 10) + (save[i++] - '0');
+	}
+	return (i);
+}
